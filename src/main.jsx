fb01adebdb50246882795150322787290432098d
kaismarzougui2-cyber <kais.marzougui2@gmail.com>
2026-02-25 18:28:31 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
+
+// Cache le splash screen une fois React monté
+if (window.__hideSplash) {
+  window.__hideSplash()
+}
